dc076b3e2e28c7c202dfff3e2137d0755ee91554
Dominik Sträßer <dos19k1m@fh-zwickau.de>
2020-10-19 18:13:49 +0200

    umbennant und verschoben

--- GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/GradeCalculatorBaitakovaStraesserApplication.java
+++ GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/GradeCalculatorBaitakovaStraesserApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package whz.pti.eva.gradeCalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
