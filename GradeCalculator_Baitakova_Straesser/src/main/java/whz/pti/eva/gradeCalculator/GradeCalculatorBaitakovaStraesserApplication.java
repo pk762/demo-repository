aafad15a9dd218772966a13a1a90b120bcb231bc
Dominik Sträßer <dos19k1m@fh-zwickau.de>
2020-10-26 15:51:42 +0100

    init() Methode angelegt.

--- GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/GradeCalculatorBaitakovaStraesserApplication.java
+++ GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/GradeCalculatorBaitakovaStraesserApplication.java
@@ -1,7 +1,9 @@
 package whz.pti.eva.gradeCalculator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GradeCalculatorBaitakovaStraesserApplication {
@@ -9,5 +11,11 @@ public class GradeCalculatorBaitakovaStraesserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCalculatorBaitakovaStraesserApplication.class, args);
 	}
-
+	
+	@Bean
+    CommandLineRunner init() {
+        return (evt) -> {
+        	System.out.println("Nur ein kleiner Test");
+        };
+	}
 }
