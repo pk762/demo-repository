32d7b868a7bf0239bb6a96e785bb543be20cb3fd
aib18j35 <aiana.baitakova.j35@fh-zwickau.de>
2020-11-02 14:32:49 +0100

    added:htnl templates

--- GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/grade/GradeController.java
+++ GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/grade/GradeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import whz.pti.eva.gradeCalculator.grade.domain.Grade;
@@ -26,5 +27,16 @@ public class GradeController {
 			model.addAttribute("fromUser", from);
 			model.addAttribute("currentUser", from);
 			return "posting";
+	}
+	
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addGrade(@RequestParam String from, @RequestParam String to,
+	@RequestParam String lecture, @RequestParam String grade, Model model) {
+		model.addAttribute("fromUser", from);
+		model.addAttribute("currentUser", from);
+		List<Grade> gradesList = gradeService.listAllgrades();
+		model.addAttribute("listAllGrades", gradesList);
+		gradeService.addGrade(lecture, grade);
+		return "redirect:posts?from="+ from + "&to=" + to;
 	}
 }
