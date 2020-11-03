a454b224142d604d60183c1a3b0325cc099bd9ca
Dominik Sträßer <dominik.straesser.k1m@fh-zwickau.de>
2020-11-03 01:49:52 +0100

    Datenbank mit einigen einträgen initialisieren

---
+++ GradeCalculator_Baitakova_Straesser/src/main/java/whz/pti/eva/gradeCalculator/config/initialzeDB.java
@@ -0,0 +1,32 @@
+package whz.pti.eva.gradeCalculator.config;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import whz.pti.eva.gradeCalculator.grade.domain.Grade;
+import whz.pti.eva.gradeCalculator.grade.domain.GradeRepository;
+
+@Component
+public class initialzeDB {
+	private static final Logger log = LoggerFactory.getLogger(initialzeDB.class);
+	@Autowired
+	GradeRepository gradeRepo;
+	
+	@PostConstruct
+	public void init() {
+		// TODO Auto-generated method stub
+		log.debug(" >>> Db initialized");
+		Grade g1 = new Grade("EVA", "1");
+		Grade g2 = new Grade("GdP-1", "2");
+		Grade g3 = new Grade("GdP-2", "3");
+		gradeRepo.save(g1);
+		gradeRepo.save(g2);
+		gradeRepo.save(g3);
+	}
+	
+	
+}
